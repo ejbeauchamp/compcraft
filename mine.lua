42e7d13a968f667070918d92285402e8e8e73344
EJ Beauchamp <you@example.comej.beauchamp@gmail.com>
2017-09-06 20:22:38 -0700

    added fuel check and print

--- mine.lua
+++ mine.lua
@@ -8,6 +8,11 @@ end
 local targetLength = tonumber(args[1]);
 local turnLeft = args[2] == "true";
 
+if turtle.getFuelCount() < targetLength*4+50 then
+  print("Not enough fuel!");
+  return;
+end
+
 local turnCount = 0;
 while turnCount < 4 do
 
@@ -15,11 +20,16 @@ while turnCount < 4 do
   while distanceCount < targetLength do
     if turtle.forward() then
       distanceCount = distanceCount + 1;
+      print("forward!");
     else
+      print("dig!");
       turtle.dig();
     end
   end
 
   turnCount = turnCount + 1;
+  turtle.turnLeft();
+  print("left!");
+  
 end
 
