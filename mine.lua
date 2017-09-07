5dcb781d2d47f0af8124b8a3e8244a9d47950fa5
EJ Beauchamp <you@example.comej.beauchamp@gmail.com>
2017-09-06 20:40:53 -0700

    added torch placement

--- mine.lua
+++ mine.lua
@@ -13,20 +13,42 @@ if turtle.getFuelCount() < targetLength*4+50 then
   return;
 end
 
+-- Loop through each stretch
 local turnCount = 0;
 while turnCount < 4 do
 
+  -- Loop through each block in the stretch
   local distanceCount = 1;
   while distanceCount < targetLength do
+
+    -- Place an item behind us
+    if distanceCount == 2 then
+      turtle.turnLeft();
+      turtle.turnLeft();
+      turtle.place();
+      turtle.turnLeft();
+      turtle.turnLeft();
+    end
+
+    -- Check if we are nearly done
+    if (distanceCount == targetLength - 1) and turnCount == 4 then
+      turtle.digUp();
+      turtle.place();
+      return;
+    end
+
+    -- Try to move forward
     if turtle.forward() then
       distanceCount = distanceCount + 1;
       print("forward!");
     else
+      -- Couldn't move forward, lets dig
       print("dig!");
       turtle.dig();
     end
   end
 
+  -- Turn!
   turnCount = turnCount + 1;
   turtle.turnLeft();
   print("left!");
